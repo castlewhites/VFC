73a0f4eb25822b7442cb767bdc0208ff049eef6a
kevcast95 <kevcast06@gmail.com>
2022-04-25 23:13:12 -0500

    adding hb pages

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const { store, persistor } = configureStore()
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App/>
       </BrowserRouter>
     </PersistGate>
